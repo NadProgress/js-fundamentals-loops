b34feeabc2b2fb43d26548b365f70a43b2b750da
Nkusi Aime Didier <nkuaime@gmail.com>
2024-04-25 21:22:48 +0200

    completed fundamentals loops

--- src/for-loop-and-arrays.js
+++ src/for-loop-and-arrays.js
@@ -6,10 +6,15 @@ let word = '' // eslint-disable-line prefer-const
 // TODO: Add code below this line to make the tests pass
 
 // 1. Use a for loop to set the sum variable to the sum of all the values in nums
-
+for (let index = 0; index < nums.length; index++) {
+  sum += index
+}
 // 2. Use a for loop to populate doubledNums with every value from nums array doubled (i.e [2, 6, 24, etc...])
 const doubledNums = []
-
+for (let index = 0; index < nums.length; index++) {
+  const normalNumber = nums[index]
+  doubledNums.push(normalNumber * 2)
+}
 // 3. Use a for loop to set word variable equal to all the letters in the letters array combined as a single string
 
 // 4. Use a for loop to populate everySecondNum with every second number from the nums array
